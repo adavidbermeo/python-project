29e1f2a9ad667fdae3080c8f22e1a75ca67a0caa
Jonathan Garizado <jonathangari16@gmail.com>
2021-05-15 19:48:15 -0500

    Ejercicios 7 y 8

---
+++ Prube.py
@@ -0,0 +1,36 @@
+print ('Hola Mundo')
+
+numero = 10
+print (numero)
+print(type (numero))
+
+"""Igual que el programa anterior, pero esta vez la pirámide debe aparecer
+invertida, con el vértice hacia abajo.
+"""
+
+print('*********')
+print(' *     *')
+print('  *   *')
+print('    * ')
+
+"""
+Escribir un programa que pinte por pantalla alguna escena - el campo, la
+habitación de una casa, un aula, etc.,-o algún objeto animado o inanimado un
+carro, un gato, una taza de café, etc. Debe tener en cuenta que puedes utilizar
+caracteres como *, +, <, #, @, etc. ¡Échale imaginación!
+
+"""
+print('   /~\\')
+print('  | oo) .')
+print('  _\\=/_') 
+print(' /   _  \\')
+print('// |/ \| \\')
+print('\\\\  \_/  ||')
+print(' \\ |\ /| ||')
+print('  # _ _/  #')
+print('   | | |')
+print('   | | |')
+print('   []|[]')
+print('   | | |')
+print('  / ] [ \\')
+print('____________________')
