f75f88d917c15f17432a26efd54fabb065a10a74
sebastianbermeo <sebastianbermeo1308@gmail.com>
2021-06-12 18:51:55 -0500

    cambio·2

--- angel.py
+++
@@ -1,8 +0,0 @@
-dias_semana = ['Lunes','Martes','Miercoles','Jueves','Viernes']
-materias = ['Matematicas','Español','Fisica','Quimica','Calculo Integral']
-
-for i in range(0,5):
-    print(dias_semana[i], ":", materias[i])
-
-for i in range(0,5):
-    
--- juan.py
+++ juan.py
@@ -1,3 +1,8 @@
-print("Sebastian")
-print("3505227448")
-print("Rivera Huila")
+numero = int(input("Digite un numero:"))
+if numero>0:
+    print("El numero es positivo")
+else:
+    print("El numero es negativo")
+elif numero==0:
+    print("El numero es cero")
+print("Fin")
